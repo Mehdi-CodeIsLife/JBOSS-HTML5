c37035157a208223e0145c915970d4ee0083a27c
Ritch_Chan <ritchchan@hotmail.com>
2012-09-11 16:12:56 -0400

    - add members, then it can list all members.

--- src/main/webapp/resources/js/showMessage.js
+++ src/main/webapp/resources/js/showMessage.js
@@ -1,7 +1,8 @@
+
 showMessage = function(message) {
     jQuery('<div />', {
         'class' : 'rf-p-hdr'
-    }).prependTo('#newMemberMessages').text(message).fadeOut(3000, function() {
+    }).prependTo('#newMemberMessages').text(message).fadeOut(20000, function() {
         $(this).remove();
     });
 };
