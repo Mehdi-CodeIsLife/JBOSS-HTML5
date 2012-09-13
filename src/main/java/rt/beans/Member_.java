d8ac6648e8007399962f1d1afcb3213c3b57e690
Ritch_Chan <ritchchan@hotmail.com>
2012-09-13 17:25:53 -0400

    - temporarily save

--- src/main/java/rt/beans/Member_.java
+++ src/main/java/rt/beans/Member_.java
@@ -4,11 +4,11 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2012-09-11T14:02:15.750-0400")
+@Generated(value="Dali", date="2012-09-13T17:23:31.625-0400")
 @StaticMetamodel(Member.class)
 public class Member_ {
 	public static volatile SingularAttribute<Member, Long> id;
-	public static volatile SingularAttribute<Member, String> name;
 	public static volatile SingularAttribute<Member, String> email;
+	public static volatile SingularAttribute<Member, String> name;
 	public static volatile SingularAttribute<Member, String> phoneNumber;
 }
