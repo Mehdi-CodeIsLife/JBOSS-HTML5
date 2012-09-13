a6a02e10c69ce98e9351fc65ffe863fa0f290313
Ritch_Chan <ritchchan@hotmail.com>
2012-09-13 17:51:32 -0400

    - revised the member

---
+++ src/main/java/rt/beans/Member_.java
@@ -0,0 +1,13 @@
+package rt.beans;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2012-09-13T17:26:57.046-0400")
+@StaticMetamodel(Member.class)
+public class Member_ {
+	public static volatile SingularAttribute<Member, Long> id;
+	public static volatile SingularAttribute<Member, String> email;
+	public static volatile SingularAttribute<Member, String> password;
+}
