7a64206a0f2e1a633c2ea7df8f3d14fdcb2225ee
Lin Yi Zhao <zhaolinyi@gmail.com>
2012-09-14 23:24:43 -0400

    Move all root files to /RichFacesProject folder, no changes in source files themself.

---
+++ RichFacesProject/src/test/java/BookbankTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import org.junit.Test;
+
+import rt.beans.Bookbank;
+
+
+public class BookbankTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
